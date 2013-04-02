c90a3d5bb506566c743fb2578e8bb31276706747
Constantine Antonakos <con.antonakos@gmail.com>
2013-04-02 17:22:32 -0400

    Delete files

--- demo_rails_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoRailsApp::Application.config.secret_token = '11d8c83f3b85a8184bd7925f3ebf384c4786a62823ca31c3a110a1fa86e497e78cc10a785db66b0739bcdf02f4a866836ffa5e00eb89cef0ed00dda55fa7dde7'
